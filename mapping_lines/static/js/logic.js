ed1705469b5ba1ed551b31e94cb99c186ae2e23f
ErmaSwartz <ermahswartz@gmail.com>
2021-02-26 13:07:06 -0700

    data

--- mapping_lines/static/js/logic.js
+++ mapping_lines/static/js/logic.js
@@ -41,3 +41,22 @@ L.polyline(line, {
     color: "yellow"
   }).addTo(map);
 
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
